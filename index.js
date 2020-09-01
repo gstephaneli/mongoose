dbca2c9d01951158157adaf5d6a5333d50d85450
Guilherme Stephaneli <guilhermesthephaneli507@gmail.com>
2020-08-31 22:03:19 -0300

    Adicionando variaveis de ambiente

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import express from 'express'
 import mongoose from 'mongoose'
+import dotenv from 'dotenv'
 
 import { registerRouter } from './routers/registerRouter.js'
 
+dotenv.config()
+
 const conectionInBD = async() =>{
     try {
-         await mongoose.connect('mongodb+srv://adminbd:mongo123@cluster0.r196i.mongodb.net/Banks?retryWrites=true&w=majority', {
-             useNewUrlParser: true,
+        await mongoose.connect('mongodb+srv://'+ process.env.USERDB +':'+ process.env.PASSDB +'@cluster0.r196i.mongodb.net/Banks?retryWrites=true&w=majority', {
+            useNewUrlParser: true,
              useUnifiedTopology: true,
              useFindAndModify: false,
              useCreateIndex: true
